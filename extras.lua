cedf69c521c6157f1ddb97e10723a2adb5c0c39a
generalwrex <1731139+generalwrex@users.noreply.github.com>
2022-02-06 14:35:04 -0800

    inital commit

---
+++ extras.lua
@@ -0,0 +1,24 @@
+-- Author      : generalwrex (Natop on Myzrael)
+-- Create Date : 2/6/2022 10:35:32 AM
+
+slotNames = {
+    "HeadSlot",
+    "NeckSlot",
+    "ShoulderSlot",
+    "BackSlot",
+    "ChestSlot",
+    "ShirtSlot",
+    "TabardSlot",
+    "WristSlot",
+    "HandsSlot",
+    "WaistSlot",
+    "LegsSlot",
+    "FeetSlot",
+    "Finger0Slot",
+    "Finger1Slot",
+    "Trinket0Slot",
+    "Trinket1Slot",
+    "MainHandSlot",
+    "SecondaryHandSlot",
+    "AmmoSlot"
+}
